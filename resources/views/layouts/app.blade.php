92152d9f153c3dc5c71d0205278bd0229949c333
Fedi Khatib <fedikhatib1@gmail.com>
2020-08-08 15:07:24 +0100

    adding blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,6 +13,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+    
+    
+
 </head>
 <body>
     <div id="app">
@@ -76,5 +81,11 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('js')
+    
+    
+    
 </body>
+
+
 </html>
